5d00cad29b8d6cf9a2f1db64a748304d09cbe7f9
Tim Ritter <timrittermail@gmail.com>
2023-03-12 21:08:46 +0100

    Add rudimentary tests

---
+++ tests/cli.rs
@@ -0,0 +1,70 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+use regex::Regex;
+
+fn parse_help_string(help_str: &str) -> Vec<String> {
+    let blacklisted_commands = vec!["help", "edit", "execute"];
+    let re = Regex::new(r"^\s+(\w+)\s+.*$").unwrap();
+    let mut commands = vec![];
+    for line in help_str.lines() {
+        if let Some(captures) = re.captures(line) {
+            commands.push(captures[1].to_string());
+        }
+    }
+
+    commands.retain(|command| !blacklisted_commands.contains(&command.as_str()));
+
+    commands
+}
+
+#[test]
+fn help_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("p")?;
+
+    cmd.arg("help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Usage: p <COMMAND>"));
+
+    Ok(())
+}
+
+#[test]
+fn subcommands_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut main_cmd = Command::cargo_bin("p")?;
+    main_cmd.arg("help");
+    let main_output = main_cmd.output().expect("Failed to execute command");
+    let main_output_string = String::from_utf8(main_output.stdout).unwrap();
+    let main_subcommands = parse_help_string(&main_output_string);
+
+    let mut repo_cmd = Command::cargo_bin("p")?;
+    repo_cmd.arg("repo").arg("help");
+    let repo_output = repo_cmd.output().expect("Failed to execute command");
+    let repo_output_string = String::from_utf8(repo_output.stdout).unwrap();
+    let repo_subcommands = parse_help_string(&repo_output_string);
+
+    let subcommands = main_subcommands
+        .iter()
+        .chain(repo_subcommands.iter())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    println!("Subcommands: {:?}", subcommands);
+
+    for subcommand in subcommands {
+        let mut cmd = Command::cargo_bin("p")?;
+
+        cmd.arg(subcommand);
+
+        let assert_success = cmd.status().expect("Failed to execute command").success();
+
+        if !assert_success {
+            cmd.assert().failure().stderr(predicate::str::contains("Usage"));
+        } else {
+            cmd.assert().success();
+        }
+    }
+
+    Ok(())
+}
